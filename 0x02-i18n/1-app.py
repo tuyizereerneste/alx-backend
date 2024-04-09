a71a97e08d7d61513d5e4125ccf7805f0efd63c7
tuyizereerneste <tuyizereernest@yahoo.com>
2024-04-09 13:25:25 +0200

    starting babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """
+    class to configure babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ define / route
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port="5000", host="0.0.0.0", debug=True)
